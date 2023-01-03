24777944c48067507249f4b65e8d295ac8f9827c
ElvesJohn <95068415+ElvesJohn@users.noreply.github.com>
2023-01-03 01:09:14 +0000

    Add files via upload

--- 30062549PythonCW.py
+++ 30062549PythonCW.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
+import statsmodels as sm
 import plotly.express as px
 import seaborn as sns
 #from wordcloud import WordCloud, STOPWORDS
